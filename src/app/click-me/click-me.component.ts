3d4511122ad6a43500dc63aaf85a800f8139d285
Enda Lee <enda.lee@ittdublin.ie>
2016-11-07 11:25:37 +0000

    Input working!

---
+++ src/app/click-me/click-me.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-me',
+  templateUrl: './click-me.component.html',
+  styleUrls: ['./click-me.component.css']
+})
+export class ClickMeComponent implements OnInit {
+
+clickMessage: string = "";
+
+  constructor() { }
+
+  onclickMe(){
+    this.clickMessage = "You've clickedthe button!";
+  }
+
+  ngOnInit() {
+  }
+}
+  
+
+
